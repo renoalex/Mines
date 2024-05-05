d9bf6e4a368d51c89228ad2c34e469e23f013380
Reno <alexrenovaidiyan@gmail.com>
2024-05-06 00:33:39 +0530

    created build folder

---
+++ build/CMakeFiles/mines.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mines.dir/main.cpp.o"
+  "CMakeFiles/mines.dir/main.cpp.o.d"
+  "mines"
+  "mines.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mines.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
